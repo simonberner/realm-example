089b255b6c25a8ff38981d8a44f8368cb400e637
Simon Berner <sibebzh@gmail.com>
2022-03-24 16:49:52 +0100

    add(view): :boom: add the @AutoOpen to the RealmOpeningView

--- RealmExample/Views/RealmOpeningView.swift
+++ RealmExample/Views/RealmOpeningView.swift
@@ -6,10 +6,36 @@
 //
 
 import SwiftUI
+import RealmSwift
 
 struct RealmOpeningView: View {
+
+    // always need newest data, does not work offline
+//    @AsyncOpen(appId: "application-0-ezdqk", partitionValue: "", timeout: 4000) var asyncOpen
+
+    // always show data also offline
+    @AutoOpen(appId: "application-0-ezdqk", partitionValue: "", timeout: 4000) var realmOpen
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+        switch realmOpen {
+            case .connecting:
+                ProgressView("connecting ...")
+
+            case .waitingForUser:
+                ProgressView("waiting for user ...")
+
+            case .progress(let progress):
+                ProgressView(progress)
+                    .padding(50)
+
+            case .open(let realm):
+                GroupLoadingView()
+                    .environment(\.realm, realm) // the view and all its child-views shall use this realm database
+
+            case .error(let error):
+                Text("opening realm error: \(error.localizedDescription)")
+        }
     }
 }
 
